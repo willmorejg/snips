4e45a523662bcb25f60a93501e7d703dc768a1ac
Jim Willmore <willmorejg@users.noreply.github.com>
2021-02-14 11:40:51 -0500

    Create ReverseString.java

---
+++ ReverseString.java
@@ -0,0 +1,18 @@
+/*
+Reverse a Java String without using a StringBuffer or StringBuilder.
+*/
+
+public class ReverseString {
+    public static void main(String[] args) {
+        String original = "zyxwvutsrqponmlkjihgfedcba";
+        char chars[] = original.toCharArray();
+        char result[] = new char[original.length()];
+
+        for(int c = chars.length - 1; c >= 0; c--) {
+            int idx = chars.length - 1 - c;
+            result[idx] = chars[c];
+        }
+
+        System.out.println(new String(result));
+    }
+}
